6ffce07bbf6e287c678d8a0236718a43e8d5c42b
Niels Rupf <nrupf@uchicago.edu>
2020-08-25 14:39:55 +0200

    config routes established

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :medias, only: %i[index show create]
+  resources :users, only: %i[index show edit update destroy] do
+    resources :watches, only: %i[index create update destroy]
+  end
 end
