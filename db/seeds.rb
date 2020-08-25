fc0b4bd971233f632e4d7e0597faaa2eb3730385
Gerrit Scheppat <garry-007@gmx.de>
2020-08-25 18:28:27 +0200

    template for seeds and api call done

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,32 +16,21 @@ medium = JSON.parse(medium_raw)
 ## BERLIN CALLING ##
 # origin_countries = medium["production_countries"].map.with_index { |country, index| country if index < 1 }
 
+title = medium["title"] ? medium["title"] : medium["name"]
+first_air_date = medium["first_air_date"] ? medium["first_air_date"] : medium["release_date"]
+origin_countries = medium["production_countries"] ? medium["production_countries"].take(3).map { |country| country["name"] } : [medium["origin_country"]]
+
+if medium["episode_run_time"]
+  sum = 0
+  medium["episode_run_time"].each { |time| sum += time.to_i }
+  sum = sum / medium["episode_run_time"].length
+end
+
+runtime = medium["runtime"] ? medium["runtime"] : sum
 
 Medium.create(
-  title: medium["title"],
-  runtime: medium["runtime"],
-  genres: medium["genres"].take(3).map { |genre| genre["name"] },
-  description: medium["overview"],
-  language: medium["original_language"],
-  rating: medium["vote_average"],
-  # media_type: medium["notthere"],
-  imdb_id: medium["imdb_id"],
-  # total_seasons: medium["notthere"],
-  tmdb_id: medium["id"],
-  origin_countries: medium["production_countries"].take(3).map { |country| country["name"] },
-  first_air_date: medium["release_date"],
-  # last_air_date: medium["release_date"]
-  poster: "http://image.tmdb.org/t/p/w500///" + medium["poster_path"],
-)
-
-## LOTR 2 TOWERS ##
-
- medium_raw = RestClient.get "https://api.themoviedb.org/3/movie/121?api_key=8081155eac22e256dfac70b9d7913b13&language=en-US"
- medium = JSON.parse(medium_raw)
-
- Medium.create(
-  title: medium["title"],
-  runtime: medium["runtime"],
+  title: title,
+  runtime: runtime,
   genres: medium["genres"].take(3).map { |genre| genre["name"] },
   description: medium["overview"],
   language: medium["original_language"],
@@ -50,9 +39,44 @@ Medium.create(
   imdb_id: medium["imdb_id"],
   total_seasons: medium["number_of_seasons"],
   tmdb_id: medium["id"],
-  origin_countries: medium["production_countries"].take(3).map { |country| country["name"] },
-  first_air_date: medium["release_date"],
-  # last_air_date: medium["release_date"]
+  origin_countries: origin_countries,
+  first_air_date: first_air_date,
+  last_air_date: medium["last_air_date"],
+  poster: "http://image.tmdb.org/t/p/w500///" + medium["poster_path"],
+)
+
+
+## LOTR 2 TOWERS ##
+
+medium_raw = RestClient.get "https://api.themoviedb.org/3/movie/121?api_key=8081155eac22e256dfac70b9d7913b13&language=en-US"
+medium = JSON.parse(medium_raw)
+
+title = medium["title"] ? medium["title"] : medium["name"]
+first_air_date = medium["first_air_date"] ? medium["first_air_date"] : medium["release_date"]
+origin_countries = medium["production_countries"] ? medium["production_countries"].take(3).map { |country| country["name"] } : [medium["origin_country"]]
+
+if medium["episode_run_time"]
+  sum = 0
+  medium["episode_run_time"].each { |time| sum += time.to_i }
+  sum = sum / medium["episode_run_time"].length
+end
+
+runtime = medium["runtime"] ? medium["runtime"] : sum
+
+Medium.create(
+  title: title,
+  runtime: runtime,
+  genres: medium["genres"].take(3).map { |genre| genre["name"] },
+  description: medium["overview"],
+  language: medium["original_language"],
+  rating: medium["vote_average"],
+  # media_type: medium["notthere"],
+  imdb_id: medium["imdb_id"],
+  total_seasons: medium["number_of_seasons"],
+  tmdb_id: medium["id"],
+  origin_countries: origin_countries,
+  first_air_date: first_air_date,
+  last_air_date: medium["last_air_date"],
   poster: "http://image.tmdb.org/t/p/w500///" + medium["poster_path"],
 )
 
@@ -66,7 +90,7 @@ medium = JSON.parse(medium_raw)
 
 title = medium["title"] ? medium["title"] : medium["name"]
 first_air_date = medium["first_air_date"] ? medium["first_air_date"] : medium["release_date"]
-origin_countries = medium["production_countries"] ? medium["production_countries"] : medium["origin_country"]
+origin_countries = medium["production_countries"] ? medium["production_countries"].take(3).map { |country| country["name"] } : [medium["origin_country"]]
 
 if medium["episode_run_time"]
   sum = 0
@@ -74,7 +98,7 @@ if medium["episode_run_time"]
   sum = sum / medium["episode_run_time"].length
 end
 
-runtime = medium["runtime"] ? medium["runtime"] : run
+runtime = medium["runtime"] ? medium["runtime"] : sum
 
 Medium.create(
   title: title,
@@ -87,7 +111,7 @@ Medium.create(
   imdb_id: medium["imdb_id"],
   total_seasons: medium["number_of_seasons"],
   tmdb_id: medium["id"],
-  origin_countries: origin_countries.take(3).map { |country| country["name"] },
+  origin_countries: origin_countries,
   first_air_date: first_air_date,
   last_air_date: medium["last_air_date"],
   poster: "http://image.tmdb.org/t/p/w500///" + medium["poster_path"],
