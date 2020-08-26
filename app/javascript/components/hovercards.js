31773938bb42b8afc863c3a0cd0129a73f4475b5
Jeroen Zwart <jeroen.zwart@live.com>
2020-08-26 17:28:35 +0200

    made improvements to eventlistener and css

--- app/javascript/components/hovercards.js
+++ app/javascript/components/hovercards.js
@@ -1,11 +1,17 @@
 let hover = document.querySelectorAll(".movieposter");
 let popup = document.querySelector(".popup");
 
-
 hover.forEach(movie => {
   movie.addEventListener("mouseover", function( event ) {
   // highlight the mouseenter target
-  console.log(event.target.nextElementSibling)
   event.target.nextElementSibling.style.display = "block";
+  movie.style.transform = "scale(1.5)";
+  movie.style.zIndex = "99";
+  });
+  movie.addEventListener("mouseout", function( event ) {
+  // highlight the mouseenter target
+  event.target.nextElementSibling.style.display = "none";
+  movie.style.transform = "scale(1)";
+  movie.style.zIndex = "initial";
   });
 });
