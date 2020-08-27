92148fd6c3c998902c2c526ff3017497546cb95c
Gerrit Scheppat <garry-007@gmx.de>
2020-08-27 17:55:36 +0200

    handled case of no search result

--- app/javascript/channels/media_searchbar.js
+++ app/javascript/channels/media_searchbar.js
@@ -1,32 +1,32 @@
 const fetchMedia = (query) => {
-  const results = document.querySelector("#results");
-  const form = document.querySelector("template");
-  const TMDB_KEY = results.dataset.tmdbApiKey;
+  const results = document.querySelector("#results"); // results is ul from the navbar where results get inserted as li
+  const form = document.querySelector("template"); // from navbar
+  const TMDB_KEY = results.dataset.tmdbApiKey; // takes the tmdb key from the ul dataset
   fetch(`https://api.themoviedb.org/3/search/multi?api_key=${TMDB_KEY}&query=${query}`)
   .then(response => response.json())
   .then((data) => {
-    if (data.results) {
+    if (data.results.length !== 0) { // if no results we get an empty array
       data.results.slice(0, 5).reverse().forEach((result) => {
         const name = result.title ? result.title : result.name;
-        const medium = form.innerHTML;
-        results.insertAdjacentHTML("afterbegin", medium);
-        const newForm = document.querySelector(".media-form");
-        const idInput = newForm.querySelector("#tmbd_id");
+        const medium = form.innerHTML; // takes the inner html of the template in navbar
+        results.insertAdjacentHTML("afterbegin", medium); // puts the template html in the ul
+        const newForm = document.querySelector(".media-form"); // query for the template to actually have access to the html inside
+        const idInput = newForm.querySelector("#tmdb_id");
         idInput.value = result.id;
         const image = newForm.querySelector("img");
         image.src = result.poster_path ? `http://image.tmdb.org/t/p/w500///${result.poster_path}`: "https://image.flaticon.com/icons/svg/16/16980.svg";
         const title = document.querySelector("#media-name");
         title.innerText = name;
-      };
-    } else {
-      results.innerText= "No search results");
+      });
+    } else { // if we get an empty array / no results
+      const medium = form.innerHTML; // se above
+      results.insertAdjacentHTML("afterbegin", medium);
+      const image = results.querySelector("img");
+      image.remove();
+      const title = document.querySelector("#media-name");
+      title.innerText = "no search results";
     };
-    );
-  });
+  })
 };
 
 export { fetchMedia };
-
-
-// if data.results do stuff
-// else display error as list element
