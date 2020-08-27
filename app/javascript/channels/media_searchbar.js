c8985f7c5ee205d9c5b7d94411beb62cc92d06a1
Gerrit Scheppat <garry-007@gmx.de>
2020-08-27 15:28:15 +0200

    black image added and started styling results search

--- app/javascript/channels/media_searchbar.js
+++ app/javascript/channels/media_searchbar.js
@@ -1,4 +1,3 @@
-// const apiKey = "<%= ENV['TMDB_KEY'] %>";
 const fetchMedia = (query) => {
   const results = document.querySelector("#results");
   const form = document.querySelector("template");
@@ -10,13 +9,13 @@ const fetchMedia = (query) => {
     data.results.slice(0, 5).reverse().forEach((result) => {
       console.log(result)
       const name = result.title ? result.title : result.name;
-      const medium = form.innerHTML;
+      const medium = form.innerHTML; 
       results.insertAdjacentHTML("afterbegin", medium);
-      const newForm = document.querySelector(".media-form");
+      const newForm = document.querySelector(".media-form"); // why not use old form?
       const idInput = newForm.querySelector("#tmbd_id");
       idInput.value = result.id;
       const image = newForm.querySelector("img");
-      image.src = `http://image.tmdb.org/t/p/w500///${result.poster_path}`;
+      image.src = result.poster_path ? `http://image.tmdb.org/t/p/w500///${result.poster_path}`: "https://image.flaticon.com/icons/svg/16/16980.svg";
       const title = document.querySelector("#media-name");
       title.innerText = name;
     });
