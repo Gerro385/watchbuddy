5a02cdcf4178ff4c746768ea9b980ead04566cb6
Gerrit Scheppat <garry-007@gmx.de>
2020-08-28 11:46:34 +0200

    422 error with js fixed

--- app/javascript/channels/media_searchbar.js
+++ app/javascript/channels/media_searchbar.js
@@ -3,33 +3,33 @@ const fetchMedia = (query) => {
   const form = document.querySelector("template"); // from navbar
   const TMDB_KEY = results.dataset.tmdbApiKey; // takes the tmdb key from the ul dataset
   const searchbar = document.querySelector('#search-media');
-  fetch(`https://api.themoviedb.org/3/search/multi?api_key=${TMDB_KEY}&query=${query}`)
-  .then(response => response.json())
-  .then((data) => {
-    if (searchbar.value !== "") {
-    if (data.results.length !== 0) { // if no results we get an empty array
-      data.results.slice(0, 5).reverse().forEach((result) => {
-        const name = result.title ? result.title : result.name;
-        const medium = form.innerHTML; // takes the inner html of the template in navbar
-        results.insertAdjacentHTML("afterbegin", medium); // puts the template html in the ul
-        const newForm = document.querySelector(".media-form"); // query for the template to actually have access to the html inside
-        const idInput = newForm.querySelector("#tmdb_id");
-        idInput.value = result.id;
-        const image = newForm.querySelector("img");
-        image.src = result.poster_path ? `http://image.tmdb.org/t/p/w500///${result.poster_path}`: "https://image.flaticon.com/icons/svg/16/16980.svg";
+  if (searchbar.value !== "") {
+    fetch(`https://api.themoviedb.org/3/search/multi?api_key=${TMDB_KEY}&query=${query}`)
+    .then(response => response.json())
+    .then((data) => {
+      if (data.results.length !== 0) { // if no results we get an empty array
+        data.results.slice(0, 5).reverse().forEach((result) => {
+          const name = result.title ? result.title : result.name;
+          const medium = form.innerHTML; // takes the inner html of the template in navbar
+          results.insertAdjacentHTML("afterbegin", medium); // puts the template html in the ul
+          const newForm = document.querySelector(".media-form"); // query for the template to actually have access to the html inside
+          const idInput = newForm.querySelector("#tmdb_id");
+          idInput.value = result.id;
+          const image = newForm.querySelector("img");
+          image.src = result.poster_path ? `http://image.tmdb.org/t/p/w500///${result.poster_path}`: "https://image.flaticon.com/icons/svg/16/16980.svg";
+          const title = document.querySelector("#media-name");
+          title.innerText = name;
+        });
+      } else { // if we get an empty array / no results
+        const medium = form.innerHTML; // se above
+        results.insertAdjacentHTML("afterbegin", medium);
+        const image = results.querySelector("img");
+        image.remove();
         const title = document.querySelector("#media-name");
-        title.innerText = name;
-      });
-    } else { // if we get an empty array / no results
-      const medium = form.innerHTML; // se above
-      results.insertAdjacentHTML("afterbegin", medium);
-      const image = results.querySelector("img");
-      image.remove();
-      const title = document.querySelector("#media-name");
-      title.innerText = "no search results";
-    };
+        title.innerText = "no search results";
+      };
+    })
   };
-  })
 };
 
 export { fetchMedia };
