04d4f893218c8c65ef7aa1236c793d5a0df1ea50
Gerrit Scheppat <garry-007@gmx.de>
2020-08-30 10:57:03 +0200

    cleaned up the search a bit, dropped results for type person

--- app/javascript/channels/media_searchbar.js
+++ app/javascript/channels/media_searchbar.js
@@ -10,24 +10,26 @@ const fetchMedia = (query) => {
     .then((data) => {
       if (data.results.length !== 0) { // if no results we get an empty array
         data.results.slice(0, 5).reverse().forEach((result) => {
-          const name = result.title ? result.title : result.name;
-          const date = result.release_date ? result.release_date : 'No date';
-          const rating = result.vote_average ? `Rating: ${result.vote_average}` : 'No rating';
-          const medium = form.innerHTML; // takes the inner html of the template in navbar
-          results.insertAdjacentHTML("afterbegin", medium); // puts the template html in the ul
-          const newForm = document.querySelector(".media-form"); // query for the template to actually have access to the html inside
-          const idInput = newForm.querySelector("#tmdb_id");
-          idInput.value = result.id;
-          const mediaType = newForm.querySelector("#media_type");
-          mediaType.value = result.media_type;
-          const image = newForm.querySelector("img");
-          image.src = result.poster_path ? `http://image.tmdb.org/t/p/w500///${result.poster_path}`: "https://image.flaticon.com/icons/svg/16/16980.svg";
-          const title = document.querySelector("#media-name");
-          title.innerText = name;
-          const mediaDate = document.querySelector("#media-date");
-          mediaDate.innerText = date;
-          const mediaRating = document.querySelector("#media-rating");
-          mediaRating.innerText = rating;
+          if (result.media_type != "person") {
+            const name = result.title ? result.title : result.name;
+            const date = result.release_date ? result.release_date : result.first_air_date;
+            const rating = result.vote_average ? `Rating: ${result.vote_average}` : 'No rating';
+            const medium = form.innerHTML; // takes the inner html of the template in navbar
+            results.insertAdjacentHTML("afterbegin", medium); // puts the template html in the ul
+            const newForm = document.querySelector(".media-form"); // query for the template to actually have access to the html inside
+            const idInput = newForm.querySelector("#tmdb_id");
+            idInput.value = result.id;
+            const mediaType = newForm.querySelector("#media_type");
+            mediaType.value = result.media_type;
+            const image = newForm.querySelector("img");
+            image.src = result.poster_path ? `http://image.tmdb.org/t/p/w500///${result.poster_path}`: "https://image.flaticon.com/icons/svg/16/16980.svg";
+            const title = document.querySelector("#media-name");
+            title.innerText = name;
+            const mediaDate = document.querySelector("#media-date");
+            mediaDate.innerText = date;
+            const mediaRating = document.querySelector("#media-rating");
+            mediaRating.innerText = rating;
+          };
         });
       } else { // if we get an empty array / no results
         const medium = form.innerHTML; // se above
