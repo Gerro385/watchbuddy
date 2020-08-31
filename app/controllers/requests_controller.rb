e4b1c1fadfd16c18bce0e1327e47f3058798be34
Niels Rupf <nrupf@uchicago.edu>
2020-08-31 14:21:29 +0200

    ..

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -15,5 +15,8 @@ class RequestsController < ApplicationController
   end
 
   def buddies
+    sent = policy_scope(Request).where(sender_id: params[:id], status: 1)
+    received = policy_scope(Request).where(receiver_id: params[:id], status: 1)
+    @buddies = sent.map { |request| User.find_by(id: request.receiver_id) } + received.map { |request| User.find_by(id: request.sender_id) }
   end
 end
