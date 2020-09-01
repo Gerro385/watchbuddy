e773b76a901ebc1dbe17059af4701a3f326f0ee9
Jeroen Zwart <jeroen.zwart@live.com>
2020-09-01 09:52:42 +0200

    added watchlist to watches

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_082147) do
+ActiveRecord::Schema.define(version: 2020_09_01_074716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -89,6 +89,7 @@ ActiveRecord::Schema.define(version: 2020_08_31_082147) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "medium_id"
     t.bigint "user_id"
+    t.boolean "watchlist", default: false
     t.index ["medium_id"], name: "index_watches_on_medium_id"
     t.index ["user_id"], name: "index_watches_on_user_id"
   end
