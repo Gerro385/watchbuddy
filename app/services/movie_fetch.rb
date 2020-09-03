9526995362577f55946a18b3c91cc99ba39d0404
Niels Rupf <nrupf@uchicago.edu>
2020-09-03 16:17:05 +0200

    movie fetching fix

--- app/services/movie_fetch.rb
+++ app/services/movie_fetch.rb
@@ -3,12 +3,22 @@ require 'rest-client'
 class MovieFetch
   def self.medium_hash(id, type) # fetches medium info from tmdb API and returns a hash of all elements we want to store in our db with the correct keys
     vals = JSON.parse(RestClient.get("https://api.themoviedb.org/3/#{type}/#{id}?api_key=#{ENV['TMDB_KEY']}", { accept: :json }))
+
     title = vals["title"] ? vals["title"] : vals["name"]
     first_air_date = vals["first_air_date"] ? vals["first_air_date"] : vals["release_date"]
     origin_countries = vals["production_countries"] ? vals["production_countries"].take(3).map { |country| country["name"] } : [vals["origin_country"]]
+
+    if vals["episode_run_time"]
+      sum = 0
+      vals["episode_run_time"].each { |time| sum += time.to_i }
+      sum = sum / vals["episode_run_time"].length
+    end
+
+    runtime = vals["runtime"] ? vals["runtime"] : sum
+
     {
       title: title,
-      runtime: vals["runtime"],
+      runtime: runtime,
       genres: vals["genres"].take(3).map { |genre| genre["name"] },
       description: vals["overview"],
       language: vals["original_language"],
